c7dc70574f5907a01ad1ebceb0ce31e1a6a90672
adnanmugu <adnanmugu@gmail.com>
2024-08-05 21:49:30 +0700

    app: seprate ads from layouts

---
+++ app/_layouts/ads.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Ads = () => {
+  return (
+    <div className="ads">
+      <div className="ads__164">
+        <h1>Auto Increment Generator.</h1>
+        <p>
+          Free tools for generate a list of a number that can use for NCR bill
+        </p>
+      </div>
+      <div className="ads__280">this vertical</div>
+    </div>
+  );
+};
+
+export default Ads;
