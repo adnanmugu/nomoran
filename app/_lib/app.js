380f524d09bd59c5e75023bcca3cf6a0a7b5d548
adnanmugu <adnanmugu@gmail.com>
2024-08-09 22:27:58 +0700

    add init function

--- app/_lib/app.js
+++ app/_lib/app.js
@@ -1,2 +1,20 @@
 import Files from './files.js';
 import Sequences from './sequences.js';
+
+export default function initilizeFile(data) {
+  const { fileName, type, fileCount, rows, cols, paddNum, firstValue } = data;
+
+  const proj = new Sequences(
+    fileName,
+    type,
+    fileCount,
+    rows,
+    cols,
+    firstValue,
+    paddNum
+  );
+
+  // const rowsVal = proj.getrowsValue();
+  const sequence = proj.getSequence(proj.getrowsValue());
+  return sequence;
+}
