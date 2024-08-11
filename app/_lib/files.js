4856a31bf9d42aca9660bcec98451e4ae31648be
adnanmugu <adnanmugu@gmail.com>
2024-08-11 21:50:03 +0700

    app: update method generateCsv of Files class

--- app/_lib/files.js
+++ app/_lib/files.js
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import { stringify } from 'csv-stringify';
 export default class Files {
   /**
    *
@@ -18,39 +20,43 @@ export default class Files {
     return this.fileCount;
   }
 
-  generateCsv(data, filename) {
-    // Transform the array into columns for CSV
-    // Determine the maximum length of the sub-arrays
-    const head = [];
-    const transformedArray = [];
-    const maxLength = Math.max(...data.map((arr) => arr.length));
+  /**
+   * Generates CSV files from a multidimensional array, where each sub-array
+   * corresponds
+   * to a separate CSV file. The function dynamically generates headers based
+   * on the number
+   * of rows in each sub-array and outputs the CSV files to the specified
+   * directory.
+   *
+   * @param {Array} array - 3d array containing data for .csv files.
+   */
+  generateCsvFiles(array) {
+    array.forEach((innerArray, index) => {
+      const headers = innerArray.map((_, i) => `#${i + 1}`);
 
-    for (let i = 0; i < maxLength; i++) {
-      const row = [];
-      for (let j = 0; j < data.length; j++) {
-        row.push(data[j][i] !== undefined ? data[j][i] : '');
+      // Prepare rows for CSV
+      const rowCount = innerArray[0].length;
+      const rows = [];
+
+      for (let i = 0; i < rowCount; i++) {
+        const row = innerArray.map((array) => array[i]);
+        rows.push(row);
       }
-      transformedArray.push(row);
-    }
 
-    data.forEach((number, i) => {
-      const firstNumber = number[0];
-      const lastNumber = number[number.length - 1];
+      // Add headers as the first row
+      rows.unshift(headers);
 
-      head.push(`#${i + 1}_${firstNumber}-${lastNumber}`);
+      // Convert to CSV format
+      stringify(rows, (err, output) => {
+        if (err) {
+          console.error('Error generating CSV:', err);
+          return;
+        }
+        // Write the CSV output to a file
+        const filePath = `./output_${index}.csv`;
+        fs.writeFileSync(filePath, output);
+        console.log(`CSV file generated: ${filePath}`);
+      });
     });
-
-    // Convert the array to a CSV string with custom headers
-    const csv = convertArrayToCSV(transformedArray, {
-      header: head,
-      separator: ',',
-    });
-
-    try {
-      fs.writeFileSync('./test.csv', csv);
-      console.log('file written succesfully');
-    } catch (err) {
-      console.log(err);
-    }
   }
 }
